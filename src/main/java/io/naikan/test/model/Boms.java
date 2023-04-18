98efe73ea657ffc2a642bbf512bd77166390c61c
mnhock <martin.hock.de@gmail.com>
2023-04-18 19:24:43 +0200

    k

--- src/main/java/io/naikan/test/model/Boms.java
+++ src/main/java/io/naikan/test/model/Boms.java
@@ -8,34 +8,34 @@ public final class Boms {
   }
 
   public static InputStream validBom0asInputStream() {
-    return Boms.class.getResourceAsStream("/1.0/valid-0-bom-1.0.json");
+    return Boms.class.getResourceAsStream("/1.0/valid-0-bom.json");
   }
 
   public static InputStream validBom1asInputStream() {
-    return Boms.class.getResourceAsStream("/1.0/valid-1-bom-1.0.json");
+    return Boms.class.getResourceAsStream("/1.0/valid-1-bom.json");
   }
 
   public static InputStream validBom2asInputStream() {
-    return Boms.class.getResourceAsStream("/1.0/valid-2-bom-1.0.json");
+    return Boms.class.getResourceAsStream("/1.0/valid-2-bom.json");
   }
 
   public static InputStream invalidBom0asInputStream() {
-    return Boms.class.getResourceAsStream("/1.0/invalid-0-bom-1.0.json");
+    return Boms.class.getResourceAsStream("/1.0/invalid-0-bom.json");
   }
 
   public static InputStream invalidBom1asInputStream() {
-    return Boms.class.getResourceAsStream("/1.0/invalid-1-bom-1.0.json");
+    return Boms.class.getResourceAsStream("/1.0/invalid-1-bom.json");
   }
 
   public static InputStream invalidBom2asInputStream() {
-    return Boms.class.getResourceAsStream("/1.0/invalid-2-bom-1.0.json");
+    return Boms.class.getResourceAsStream("/1.0/invalid-2-bom.json");
   }
 
   public static InputStream invalidBom3asInputStream() {
-    return Boms.class.getResourceAsStream("/1.0/invalid-3-bom-1.0.json");
+    return Boms.class.getResourceAsStream("/1.0/invalid-3-bom.json");
   }
 
   public static InputStream invalidBom4asInputStream() {
-    return Boms.class.getResourceAsStream("/1.0/invalid-4-bom-1.0.json");
+    return Boms.class.getResourceAsStream("/1.0/invalid-4-bom.json");
   }
 }
