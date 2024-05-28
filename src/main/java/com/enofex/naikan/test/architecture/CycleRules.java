05f7bd6b8288315297c48dae121fcd3a6a43930f
mnhock <martin.hock.de@gmail.com>
2024-05-28 09:29:00 +0200

    Remove ArchUnit test

--- src/main/java/com/enofex/naikan/test/architecture/CycleRules.java
+++ src/main/java/com/enofex/naikan/test/architecture/CycleRules.java
@@ -1,6 +1,5 @@
 package com.enofex.naikan.test.architecture;
 
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
@@ -14,12 +13,6 @@ final class CycleRules {
 
   static List<DynamicTest> cycleRules(ArchUnitTestsConfig config) {
     return List.of(
-        dynamicTest("No classes should have imports from naikan model",
-            () -> noClasses()
-                .should().accessClassesThat()
-                .resideInAnyPackage("com.enofex.naikan.model")
-                .check(config.getClasses())),
-
         dynamicTest("There should be no cycles",
             () -> slices()
                 .matching(config.getNamespace() + ".(*)..")
