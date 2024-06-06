92218733c5fef2dc55795ac1b97f5e78777ee850
mnhock <martin.hock.de@gmail.com>
2024-06-06 20:57:07 +0200

    Use Taikai for architecture tests

---
+++ src/test/java/com/enofex/naikan/test/architecture/ArchitectureTest.java
@@ -0,0 +1,30 @@
+package com.enofex.naikan.test.architecture;
+
+
+import com.enofex.taikai.Taikai;
+import org.junit.jupiter.api.Test;
+
+class ArchitectureTest {
+
+  @Test
+  void shouldFulfilConstrains() {
+    Taikai taikai = Taikai.builder()
+        .namespace("com.enofex.naikan.model")
+        .test(test -> test
+            .junit5(junit5 -> junit5
+                .classesShouldNotBeAnnotatedWithDisabled()
+                .methodsShouldNotBeAnnotatedWithDisabled()))
+        .java(java -> java
+            .imports(imports -> imports
+                .shouldHaveNoCycles()
+                .shouldNotImport("..shaded..")
+                .shouldNotImport("..lombok..")
+                .shouldNotImport("org.junit.."))
+            .naming(naming -> naming
+                .classesShouldNotMatch(".*Impl")
+                .interfacesShouldNotHavePrefixI()))
+        .build();
+
+    taikai.check();
+  }
+}
